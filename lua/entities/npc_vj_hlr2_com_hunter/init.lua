e8114e27ea4d5725e0ccbf50c2f0ee182943eca4
DrVrej <DrVrej@users.noreply.github.com>
2026-09-08 09:33:46 +0500

    Update init.lua

--- lua/entities/npc_vj_hlr2_com_hunter/init.lua
+++ lua/entities/npc_vj_hlr2_com_hunter/init.lua
@@ -169,7 +169,7 @@ function ENT:FireFlechette()
 			local vel = self:RangeAttackProjVel(proj)
 			phys:Wake()
 			phys:SetVelocity(vel)
-			proj:SetAngles(vel:GetNormalized():Angle())
+			proj:SetAngles(vel:GetVelocity():Angle())
 		end
 
 		VJ.EmitSound(self, "^npc/ministrider/ministrider_fire1.wav", 105, 100)
